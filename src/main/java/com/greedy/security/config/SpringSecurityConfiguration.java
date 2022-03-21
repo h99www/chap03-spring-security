047e333f73d1ee9487a5135b8bb78716b1cf8b1d
DESKTOP-ONN8F4O\h99ww <h99www@gmail.com>
2022-03-21 12:33:22 +0900

    project init

--- src/main/java/com/greedy/security/config/SpringSecurityConfiguration.java
+++ src/main/java/com/greedy/security/config/SpringSecurityConfiguration.java
@@ -1,5 +1,27 @@
 package com.greedy.security.config;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 /* 스프링 시큐리티에 대한 설정은 따로 클래스를 생성해 한곳에서 설정한다. */
-public class SpringSecurityConfiguration {
+/* 스프링 시큐리티 설정을 위해 WebSecurityConfirurerAdapter를 상속받는다. */
+@EnableWebSecurity
+public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
 }
