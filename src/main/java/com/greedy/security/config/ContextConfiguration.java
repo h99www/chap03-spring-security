c146297d31858528aa4064dd266e0d2b49dee913
DESKTOP-ONN8F4O\h99ww <h99www@gmail.com>
2022-03-21 12:29:59 +0900

    project init

---
+++ src/main/java/com/greedy/security/config/ContextConfiguration.java
@@ -0,0 +1,9 @@
+package com.greedy.security.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.greedy.security")
+public class ContextConfiguration {
+}
---
+++ src/main/java/com/greedy/security/config/SpringSecurityConfiguration.java
@@ -0,0 +1,5 @@
+package com.greedy.security.config;
+
+/* 스프링 시큐리티에 대한 설정은 따로 클래스를 생성해 한곳에서 설정한다. */
+public class SpringSecurityConfiguration {
+}
